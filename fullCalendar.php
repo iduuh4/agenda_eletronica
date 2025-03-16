efd6a20d03c3ada77dbf7699ab04f67646e67985
Carlos Eduardo <iduuh4@gmail.com>
2025-03-15 22:46:19 -0300

    iniciando com o desenvolvimento do calendario no projeto

---
+++ fullCalendar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendário</title>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth'
+            });
+            calendar.render();
+        });
+    </script>
+</head>
+
+<body>
+    <div id='calendar'></div>
+
+
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+</body>
+
+</html>
